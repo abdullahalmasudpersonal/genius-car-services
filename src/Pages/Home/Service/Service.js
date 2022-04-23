fd27fe16cb08dfee1d0cca45df526ade5e8b70d7
masud <abdullahalmasud01737906772@gmail.com>
2022-04-23 16:17:19 +0600

    database connect and orther

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import './Service.css'
 
 const Service = ({ service }) => {
-    const { id, name, img, description, price } = service;
+    const {_id, name, img, description, price } = service;
     const navigate = useNavigate();
 
     const navigateToServiceDetail = id => {
@@ -12,10 +12,10 @@ const Service = ({ service }) => {
     return (
         <div className='service'>
             <img className='w-100' src={img} alt='' />
-            <h3>This is service: {name}</h3>
+            <h3>{name}</h3>
             <p>Price:{price}</p>
             <p><small>{description}</small></p>
-            <button onClick={() => navigateToServiceDetail(id)} className='btn btn-primary'>Book: {name}</button>
+            <button onClick={() => navigateToServiceDetail(_id)} className='btn btn-primary'>Book: {name}</button>
         </div>
     );
 };
