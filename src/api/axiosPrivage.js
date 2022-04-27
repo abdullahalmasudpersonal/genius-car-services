da41f652a2a25ee9744fcd6d8f843e6a9efdaa14
masud <abdullahalmasud01737906772@gmail.com>
2022-04-27 23:47:09 +0600

    addToken

--- src/api/axiosPrivage.js
+++ src/api/axiosPrivage.js
@@ -22,6 +22,9 @@ axiosPrivate.interceptors.response.use(function (response) {
   }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
+    if(error.response.status === 403){
+        // refresh token
+    }
     return Promise.reject(error);
   });
 
