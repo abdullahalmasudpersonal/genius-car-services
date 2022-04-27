3ec3c8d437c2fcd23376b956ea91bd8186e4b7d9
masud <abdullahalmasud01737906772@gmail.com>
2022-04-27 15:45:50 +0600

    addToken

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -6,12 +6,16 @@ import auth from '../../firebase.init';
 const Order = () => {
     const [user] = useAuthState(auth);
     const [orders, setOrders] = useState([]);
-    useEffect( () =>{
+    useEffect(() => {
 
-        const getOrders = async() =>{
+        const getOrders = async () => {
             const email = user.email;
             const url = `http://localhost:5000/order?email=${email}`;
-            const {data} = await axios.get(url);
+            const { data } = await axios.get(url, {
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            });
             setOrders(data);
         }
         getOrders();
